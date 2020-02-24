d9fa2c50afd1571f7491cf5485a80a6fc63320b9
Denis.Shubochkin <zippo1095@gmail.com>
2020-02-24 13:02:22 +0300

    diplom v2

--- public/homePage.js
+++ public/homePage.js
@@ -5,118 +5,129 @@ const logoutB = new LogoutButton();
 //(response) => console.log(response)
 
 logoutB.action = () => ApiConnector.logout(() => location.reload());
-ApiConnector.current((response) => 
-                                {
-                                    if (response.success == true) {ProfileWidget.showProfile(response.data);}
-                                });
+// ApiConnector.current((response) => 
+//                                 {
+//                                     if (response.success) {ProfileWidget.showProfile(response.data);}
+//                                 });
                           
 //ivan@demo.ru
+
+const showProfile = (responsedata) => {
+    if (!responsedata)   {
+      ApiConnector.current( response =>
+        {
+          if (response.success) {
+            ProfileWidget.showProfile(response.data);
+          }
+        } );
+      } else {
+        ProfileWidget.showProfile(responsedata);
+      }
+    }
+
+showProfile();    
+
 const ratesBoard = new RatesBoard();
 const runRate = () => ApiConnector.getStocks((response) => 
-                        {
-                            if (response.success==true) {
-                                ratesBoard.clearTable();
-                                ratesBoard.fillTable(response.data);
-                            }
-                        });
+    {
+        if (response.success) 
+            {
+                ratesBoard.clearTable();
+                ratesBoard.fillTable(response.data);
+            }
+    });
+
 runRate();                        
 setInterval(runRate,60000);
 
 const moneyManager = new MoneyManager();
-moneyManager.addMoneyCallback = (data) => { 
-                                              ApiConnector.addMoney(data,(response) =>
-                                                     {
-                                                        if (response.success==false) 
-                                                        {
-                                                           moneyManager.setMessage(true,response.data);
-                                                        }
-                                                        else 
-                                                        {
-                                                           ApiConnector.current((response) => 
-                                                        {
-                                                            if (response.success == true) {ProfileWidget.showProfile(response.data);}
-                                                        });
-                                                        moneyManager.setMessage(false,'Баланс успешно пополнен');
-                                                        }
-                                                    })            
-                                           };
+moneyManager.addMoneyCallback = (data) => 
+    { 
+        ApiConnector.addMoney(data,(response) =>
+            {
+                if (!response.success) 
+                    {
+                        moneyManager.setMessage(true,response.data);
+                    }
+                else 
+                    {                                                 
+                        moneyManager.setMessage(false,'Баланс успешно пополнен');
+                        showProfile();
+                    }
+            })            
+    };
+moneyManager.conversionMoneyCallback = (data) =>    
+    { 
+        ApiConnector.convertMoney(data,(response) =>                                                                                  
+            {
+                if (!response.success) 
+                    {
+                        moneyManager.setMessage(true,response.data);
+                    }
+                else 
+                    {                                                               
+                        moneyManager.setMessage(false,'Конвертация успешно произведена');
+                        showProfile();
+                    }                                                                              
+            });                                                                            
+    }
 
-
-moneyManager.conversionMoneyCallback = (data) =>    { 
-                                               ApiConnector.convertMoney(data,(response) =>                                                                                  
-                                                        {
-                                                            if (response.success==false) 
-                                                            {
-                                                                moneyManager.setMessage(true,response.data);
-                                                            }
-                                                            else 
-                                                            {
-                                                                ApiConnector.current((response) => 
-                                                            {
-                                                                if (response.success == true) {ProfileWidget.showProfile(response.data);}
-                                                            });
-                                                                moneyManager.setMessage(false,'Конвертация успешно произведена');
-                                                            }                                                                              
-                                                        } )                                                                            
-                                                    }
-
-moneyManager.sendMoneyCallback  = (data) => { 
-                                                ApiConnector.transferMoney(data,(response) =>                                                                                  
-                                                    {
-                                                        if (response.success==false) 
-                                                        {
-                                                            moneyManager.setMessage(true,response.data);
-                                                        }
-                                                        else 
-                                                        {
-                                                            ApiConnector.current((response) => 
-                                                        {
-                                                            if (response.success == true) {ProfileWidget.showProfile(response.data);}
-                                                        });
-                                                            moneyManager.setMessage(false,'Перевод успешно произведен');
-                                                        }                                                                              
-                                                    } )                                                                            
-                                            }                                       
+moneyManager.sendMoneyCallback  = (data) => 
+    { 
+        ApiConnector.transferMoney(data,(response) =>                                                                                  
+            {
+                if (!response.success) 
+                    {
+                        moneyManager.setMessage(true,response.data);
+                    }
+                else 
+                    {
+                        showProfile();
+                        moneyManager.setMessage(false,'Перевод успешно произведен');
+                    }                                                                              
+            });                                                                            
+    }                                       
 const favoritesWidget = new FavoritesWidget();
-ApiConnector.getFavorites(response => {
-                                        if (response.success === true) {
-                                            favoritesWidget.clearTable();
-                                            favoritesWidget.fillTable(response.data);
-                                            moneyManager.updateUsersList(response.data);
-                                        }
-                                        }
-                        );     
+const updateFavorites = (data) => 
+    {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(data);
+        moneyManager.updateUsersList(data);
+    }
+ApiConnector.getFavorites(response => 
+    {
+        if (response.success) {updateFavorites(response.data);}
+    });     
 
-favoritesWidget.addUserCallback = (data) => { 
-                                                ApiConnector.addUserToFavorites(data,(response) =>                                                                                  
-                                                {
-                                                    if (response.success==false) 
-                                                    {
-                                                        favoritesWidget.setMessage(true,response.data);
-                                                    }
-                                                    else 
-                                                    {
-                                                    favoritesWidget.clearTable();
-                                                    favoritesWidget.fillTable(response.data);
-                                                    moneyManager.updateUsersList(response.data);
-                                                    favoritesWidget.setMessage(false,'Пользователь добавлен');
-                                                    }
-                                                })
-                                            }
-favoritesWidget.removeUserCallback = (data) =>  { 
-                                                    ApiConnector.removeUserFromFavorites(data,(response) =>                                                                                  
-                                                    {
-                                                        if (response.success==false) 
-                                                        {
-                                                            favoritesWidget.setMessage(true,response.data);
-                                                        }
-                                                        else 
-                                                        {
-                                                        favoritesWidget.clearTable();
-                                                        favoritesWidget.fillTable(response.data);
-                                                        moneyManager.updateUsersList(response.data);
-                                                        favoritesWidget.setMessage(false,'Пользователь удален');
-                                                        }
-                                                    })
-                                            }                                          
+favoritesWidget.addUserCallback = (data) => 
+    { 
+        ApiConnector.addUserToFavorites(data,(response) =>                                                                                  
+            {
+                if (!response.success) 
+                    {
+                        favoritesWidget.setMessage(true,response.data);
+                    }
+                else 
+                    {
+                        updateFavorites(response.data);
+                        favoritesWidget.setMessage(false,'Пользователь добавлен');
+                    }
+            });
+    }
+
+favoritesWidget.removeUserCallback = (data) =>  
+    { 
+        ApiConnector.removeUserFromFavorites(data,(response) =>                                                                                  
+            {
+                if (!response.success) 
+                    {
+                        favoritesWidget.setMessage(true,response.data);
+                    }
+                else 
+                    {
+                        updateFavorites(response.data);
+                        favoritesWidget.setMessage(false,'Пользователь удален');
+                    }
+            });
+    }                                          
 
--- public/loginPage.js
+++ public/loginPage.js
@@ -5,20 +5,35 @@ const userForm = new UserForm();
 
 
 
-userForm.loginFormCallback = data => {ApiConnector.login(data,(response) => {if (response.success===true) {location.reload();}
-                                                                             else {alert(response.data);} })};
-userForm.registerFormCallback = data => {ApiConnector.register(data, (response) => {
-                                                                                        if (response.success===true)
-                                                                                             {
-                                                                                                alert(`Пользователь ${data.login} успешно зарегестрирован`)
-                                                                                             }
-                                                                                   
-                                                                                        else {
-                                                                                            alert(response.data);
-                                                                                        }
-                                                                                    }
-                                                              )
-                                        };
+userForm.loginFormCallback = data => 
+    {
+        ApiConnector.login(data,(response) =>   
+            {
+                if (response.success) 
+                    {
+                        location.reload();
+                    }
+                else 
+                    {
+                        userForm.setLoginErrorMessage(response.data);
+                    } 
+            });
+    };
+userForm.registerFormCallback = data => 
+    {
+        ApiConnector.register(data, (response) =>   
+            {
+                if (response.success)
+                    {                                                                                                                                                                                      
+                        userForm.setRegisterErrorMessage(`Пользователь ${data.login} успешно зарегестрирован`);
+                    }
+                                                                                    
+                else 
+                    {
+                    userForm.setRegisterErrorMessage(response.data);
+                    }
+            })
+    };
 //userForm.registerFormCallback = data => {ApiConnector.register(data, (response) => console.log(response))};
 
 
